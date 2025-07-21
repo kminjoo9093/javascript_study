6a371dca4ca49c64f52a10b214079f3599765a5b
kminjoo9093 <kminjoo9093@naver.com>
2025-07-21 17:53:52 +0900

    코드캠프 알고리즘 section02

---
+++ algorithm_basic/section02.js
@@ -0,0 +1,21 @@
+// [코드캠프] 입문자를 위한 자바스크립트 알고리즘
+
+// section02. 원두의 이력서
+function solution(a, b) {
+	let company = a;
+  company = b;
+  return company;
+}
+
+console.log(solution("캠프코드", "코드캠프"));
+console.log(solution("코딩", "딩코"));
+
+// section02. 점메추
+function solution(recommend) {
+  const menu = recommend;
+	
+	return menu;
+}
+
+console.log(solution("부대찌개")); // "부대찌개"
+console.log(solution("파스타")); // "파스타"
