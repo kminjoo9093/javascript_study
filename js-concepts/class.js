e7da14a702b6d43bb88c9bcd8d4c0ef1edcce1ef
kminjoo9093 <kminjoo9093@naver.com>
2026-06-24 00:05:34 +0900

    class 최신 문법

--- js-concepts/class.js
+++ js-concepts/class.js
@@ -6,3 +6,15 @@ function character(q, w){
 
 let nunu = new character('consume', 'snowball');
 let garen = new character('strike', 'courage');
+
+// class 구현 신문법
+// 클래스 첫글자는 대문자
+class Hero {
+  constructor(q, w){
+    this.q = q;
+    this.w = w;
+  }
+}
+
+let nunu2 = new Hero('consume', 'snowball');
+console.log(nunu2);
